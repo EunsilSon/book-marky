2162bb629fc7a8ea7af2c7a6bb713fbc89a9ea9f
EunsilSon <eunsil1023@gmail.com>
2024-06-17 00:01:43 +0900

    Init : Spring Boot 의존성 및 환경 설정

---
+++ back-end/src/main/java/com/eunsil/bookmarky/BookmarkyApplication.java
@@ -0,0 +1,13 @@
+package com.eunsil.bookmarky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarkyApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/eunsil/bookmarky/BookmarkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eunsil.bookmarky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
