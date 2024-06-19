b8be00a12e1c3b4474d2fcc67acb25247964b174
EunsilSon <eunsil1023@gmail.com>
2024-06-19 12:34:01 +0900

    Feat: 로그인 성공/실패 핸들러 추가 - AuthenticationHandler 인터페이스 구현
    issue: #2

---
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/config/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,21 @@
+package com.eunsil.bookmarky.config.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 응답 상태 코드 = 401
+        response.setContentType("text/plain;charset=UTF-8"); // 응답 값 타입
+        response.getWriter().write("Authentication Failed");
+        System.out.println("[Authentication Failed]: " + request.getParameter("username"));
+    }
+}
