536306d665014333bb4e29f4a00684e599162628
EunsilSon <eunsil1023@gmail.com>
2024-06-21 12:43:06 +0900

    Fix: Redis 토큰 유효성 검증 로직 수정 - hasKey 메서드를 사용해 반환값 바로 전달 issue: #3

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/service/ResetTokenService.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/service/ResetTokenService.java
@@ -22,8 +22,8 @@ public class ResetTokenService {
 
     /**
      * 토큰 생성 및 Redis 저장
-     * @param username
-     * @return
+     * @param username 로그인에 사용한 이메일
+     * @return 토큰
      */
     @Transactional
     public String generateToken(String username) {
@@ -39,19 +39,17 @@ public class ResetTokenService {
 
     /**
      * 토큰 유효성 검사
-     * @param token
+     * @param token 토큰
      * @return 유효성 유무
      */
     public boolean isValidToken(String token) {
-        String key = TOKEN_PREFIX + token;
-        ValueOperations<String, String> ops = redisTemplate.opsForValue();
-        String username = ops.get(key);
-        return username != null;
+        String key = TOKEN_PREFIX + token; // TOKEN_PREFIX = "password-reset-token: "
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
     }
 
     /**
      * 토큰 무효화
-     * @param token
+     * @param token 토큰
      */
     @Transactional
     public void invalidateToken(String token) {
