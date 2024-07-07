46fa188534580973ac035814177615504056cd38
EunsilSon <eunsil1023@gmail.com>
2024-07-07 11:12:31 +0900

    Fix: Security Config 수정 - 로그인 페이지 외에 경로는 인증 필요

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/config/SecurityConfig.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/config/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers("/login").authenticated() // 인증이 필요한 주소
-                        .anyRequest().permitAll())
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()) // 로그인 페이지를 제외한 모든 페이지에서 인증 필요
                 .formLogin(login -> login
                         .loginProcessingUrl("/login") // Security 가 로그인을 대신 수행하므로 컨트롤러를 만들지 않아도 됨
                         .successHandler(customAuthenticationSuccessHandler())
