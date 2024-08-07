2657bc996e23e68ca8c77e01f49cadf6dbe1b009
EunsilSon <eunsil1023@gmail.com>
2024-08-08 02:56:44 +0900

    fix: 제약 조건 수정

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/request/PasswordResetReq.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/request/PasswordResetReq.java
@@ -1,6 +1,5 @@
 package com.eunsil.bookmarky.domain.request;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,16 +7,13 @@ import lombok.Getter;
 @Getter
 public class PasswordResetReq {
 
-    @NotBlank
     @Pattern(regexp = "^[a-z\\d._]+@[a-z._]+\\.[a-z]{2,6}$") // 이메일
     private String username;
 
-    @NotBlank
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d{5,})(?=.*[~!@#$%^&*)]).{2,}$", message = "비밀번호는 최소 9자리, 최대 15자리입니다.") // 대소문자, 숫자 5개 이상, 특수문자 2개 이상
     @Size(min = 9, max = 15)
     private String password;
 
-    @NotBlank
     private String token;
 
 }
--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/vo/UserVO.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/vo/UserVO.java
@@ -1,6 +1,5 @@
 package com.eunsil.bookmarky.domain.vo;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,22 +7,18 @@ import lombok.Getter;
 @Getter
 public class UserVO {
 
-    @NotBlank
-    @Pattern(regexp = "^[a-z\\d._]+@[a-z._]+\\.[a-z]{2,6}$") // 이메일
+    @Pattern(regexp = "^[a-z\\d._]+@[a-z._]+\\.[a-z]{2,6}$", message = "아이디는 이메일 형식입니다.")
     private String username;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d{5,})(?=.*[~!@#$%^&*)]).{2,}$", message = "비밀번호는 최소 9자리, 최대 15자리입니다.") // 대소문자, 숫자 5개 이상, 특수문자 2개 이상
-    @Size(min = 9, max = 15)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d{5,})(?=.*[~!@#$%^&*)]).{2,}$", message = "비밀번호는 대소문자, 숫자 5개 이상, 특수문자 2개 이상입니다.")
+    @Size(min = 9, max = 15, message = "비밀번호는 최소 9자리, 최대 15자리입니다.")
     private String password;
 
-    @NotBlank
-    @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "닉네임은 최소 3자리, 최대 8자리입니다.") // 대소문자, 한글
-    @Size(min = 3, max = 8)
+    @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "닉네임은 영문과 한글만 가능합니다.")
+    @Size(min = 3, max = 8, message = "닉네임은 최소 3자리, 최대 8자리입니다.")
     private String nickname;
 
-    @NotBlank
-    @Pattern(regexp = "\\d{2,3}-\\d{3,4}-\\d{4}+$") // 숫자, 하이픈
+    @Pattern(regexp = "\\d{2,3}-\\d{3,4}-\\d{4}+$", message = "010-0000-0000 형식입니다.")
     private String telephone;
 
 }
