d6139336911b7fc477f924c9b5d6f5aaa757b01a
EunsilSon <eunsil1023@gmail.com>
2024-08-10 01:25:31 +0900

    fix: 생성자 어노테이션 주입 - @AllArgsConstructor 어노테이션

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/response/PasswordResetRes.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/response/PasswordResetRes.java
@@ -1,16 +1,13 @@
 package com.eunsil.bookmarky.domain.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class PasswordResetRes {
 
     private String username;
     private String token;
-
-    public PasswordResetRes(String username, String token) {
-        this.username = username;
-        this.token = token;
-    }
 
 }
