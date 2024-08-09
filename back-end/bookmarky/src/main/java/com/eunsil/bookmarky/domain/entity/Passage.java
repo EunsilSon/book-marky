fe4628b40206d64331d304464a31e4253b213ce9
EunsilSon <eunsil1023@gmail.com>
2024-08-09 18:38:54 +0900

    feat: 구절 엔티티 수정을 위한 setter 추가
    issue: #15

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/entity/Passage.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/entity/Passage.java
@@ -6,15 +6,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Builder
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
