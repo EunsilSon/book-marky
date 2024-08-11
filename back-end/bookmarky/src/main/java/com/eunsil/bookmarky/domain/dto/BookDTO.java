30b4fc6f1b2e812f89d30eb4562440e105ab1686
EunsilSon <eunsil1023@gmail.com>
2024-08-11 20:00:38 +0900

    refactor: Book 관련 로직 - Stream API 를 사용한 List 생성 - 컨트롤러 반환 값 ResponseEntity 통일 - 주석
    issue: #19

---
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/dto/BookDTO.java
@@ -0,0 +1,13 @@
+package com.eunsil.bookmarky.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+public class BookDTO {
+
+    public Long id;
+    public String title;
+
+}
