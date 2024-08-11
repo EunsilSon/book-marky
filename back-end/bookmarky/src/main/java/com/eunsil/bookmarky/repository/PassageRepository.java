767a76b67827d5e462079cf57cfe2669baee1e3a
EunsilSon <eunsil1023@gmail.com>
2024-08-11 13:53:46 +0900

    fix: 구절 조회 - 삭제 컬럼 조건 추가
    issue: #17

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/repository/PassageRepository.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/repository/PassageRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface PassageRepository extends JpaRepository<Passage, Long> {
 
-    Page<Passage> findByUserIdAndBookId(Long userId, Long bookId, Pageable pageable);
+    Page<Passage> findByUserIdAndBookIdAndIsDeleted(Long userId, Long bookId, Boolean isDeleted, Pageable pageable);
+
+    List<Passage> findByUserIdAndIsDeleted(Long userId, Boolean isDeleted);
 }
