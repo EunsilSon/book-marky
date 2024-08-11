748810eb39e9d711e3002aa087e7d0ebdb0cf377
EunsilSon <eunsil1023@gmail.com>
2024-08-11 20:36:34 +0900

    feat: BookVO 유효성 체크 추가

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/vo/BookVO.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/domain/vo/BookVO.java
@@ -1,11 +1,15 @@
 package com.eunsil.bookmarky.domain.vo;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class BookVO {
 
+    @NotNull
     private String username;
+
+    @NotNull
     private String isbn;
 
 }
