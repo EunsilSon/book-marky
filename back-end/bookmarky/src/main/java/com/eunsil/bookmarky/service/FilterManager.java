e58974b17670996aa9524f7451e199ebe4779cc7
EunsilSon <eunsil1023@gmail.com>
2024-08-11 13:50:52 +0900

    feat: 필터 매니저 클래스 생성 - Hibernate의 세션 팩토리를 이용해 트랜잭션 관리 - 필터 활성화 및 비활성화 메서드 작성
    issue: #17

---
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/service/FilterManager.java
@@ -0,0 +1,41 @@
+package com.eunsil.bookmarky.service;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FilterManager {
+
+    private final SessionFactory sessionFactory;
+
+
+    /**
+     * 필터 활성화 및 파라미터 설정
+     *
+     * @param filter 활성화 할 필터 이름
+     * @param paramName 파라미터 이름
+     * @param paramValue 파라미터 값
+     */
+    public void enableFilter(String filter, String paramName, Object paramValue) {
+        sessionFactory.inTransaction(session -> {
+            session.enableFilter(filter)
+                    .setParameter(paramName, paramValue)
+                    .validate();
+        });
+    }
+
+
+    /**
+     * 필터 비활성화
+     *
+     * @param filter 비활성화 할 필터 이름
+     */
+    public void disableFilter(String filter) {
+        sessionFactory.inTransaction(session -> {
+            session.disableFilter(filter);
+        });
+    }
+
+}
