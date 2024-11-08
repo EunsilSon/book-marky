a8c8afad39bafc501428ac909ec0b265c35bf075
EunsilSon <eunsil1023@gmail.com>
2024-11-08 15:37:35 +0900

    feat: 요청 수행 시간 로깅 - @Around 메서드로 클라이언트의 요청을 수행하는데 소요되는 시간 계산 - 요청 정보는 @Before 메서드로 실행 전에 출력
    issue: #23

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/config/aop/LogAspect.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/config/aop/LogAspect.java
@@ -1,17 +1,19 @@
 package com.eunsil.bookmarky.config.aop;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -20,38 +22,49 @@ import java.util.Objects;
 @Aspect
 @Component
 public class LogAspect {
-
     @Pointcut("execution(* com.eunsil.bookmarky..*Controller.*(..))")
-    public void controllerPointcut() {
-
+    public void controller() {
     }
 
-    @Around("controllerPointcut()")
-    public Object loggingBefore(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("controller()")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long timeMs = end - start;
+            log.info("Execution Time: {} ms", timeMs);
+        }
+    }
+
+    @Before("controller()")
+    public void logBeforeExecution(JoinPoint joinPoint) throws Throwable {
         HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
 
+        String username = request.getUserPrincipal() != null ? request.getUserPrincipal().getName() : "None";
         String controllerName = joinPoint.getSignature().getDeclaringTypeName();
         String methodName = joinPoint.getSignature().getName();
         Map<String, Object> params = new HashMap<>();
 
         try {
-            String decodedRequestURI = URLDecoder.decode(request.getRequestURI(), "UTF-8");
-
+            String decodedRequestURI = URLDecoder.decode(request.getRequestURI(), StandardCharsets.UTF_8);
             params.put("controller", controllerName);
             params.put("method", methodName);
-            //params.put("params", getParams(request));
-            //params.put("log-time", System.currentTimeMillis());
             params.put("http_method", request.getMethod());
             params.put("request_uri", decodedRequestURI);
+            params.put("user", username);
         } catch (Exception e) {
-            log.error("LoggerAspect Error", e);
+            log.error("LogAspect Error", e);
         }
 
-        log.info("[{}] {}", params.get("http_method"), params.get("request_uri"));
-        log.info("method: {}.{}", params.get("controller"), params.get("method"));
-        //log.info("params: {}", params.get("params"));
-
-        return joinPoint.proceed();
+        log.info("Request - HTTP: {} | URI: {} | User: {} | Method: {}.{}",
+                params.get("http_method"),
+                params.get("request_uri"),
+                params.get("user"),
+                params.get("controller"),
+                params.get("method"));
     }
 
 }
