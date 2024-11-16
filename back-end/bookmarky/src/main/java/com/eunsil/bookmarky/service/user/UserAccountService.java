535222e8f79d557c5f22d7bcf2e862532b7701ba
EunsilSon <eunsil1023@gmail.com>
2024-11-16 16:32:49 +0900

    refactor: 비밀번호 변경 관련 메서드 SRP 적용 - 각 메서드가 하나의 목적을 가지도록 메서드 분리 - 컨트롤러 호출 메서드는  public - 내부 비즈니스 로직 메서드는 private

--- back-end/bookmarky/src/main/java/com/eunsil/bookmarky/service/user/UserAccountService.java
+++ back-end/bookmarky/src/main/java/com/eunsil/bookmarky/service/user/UserAccountService.java
@@ -1,17 +1,12 @@
 package com.eunsil.bookmarky.service.user;
 
 import com.eunsil.bookmarky.config.SecurityUtil;
-import com.eunsil.bookmarky.domain.entity.SecureAnswer;
-import com.eunsil.bookmarky.domain.entity.SecureQuestion;
-import com.eunsil.bookmarky.domain.vo.SecureQuestionVO;
 import com.eunsil.bookmarky.domain.vo.PasswordVO;
 import com.eunsil.bookmarky.domain.entity.User;
-import com.eunsil.bookmarky.repository.user.SecureAnswerRepository;
 import com.eunsil.bookmarky.repository.user.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -21,89 +16,56 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserAccountService {
 
-    private final UserRepository userRepository;
-    private final SecureAnswerRepository secureAnswerRepository;
+    private final SecurityUtil securityUtil;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
     private final MailService mailService;
     private final ResetTokenService resetTokenService;
-    private final SecurityUtil securityUtil;
 
-    /**
-     * 비밀번호 변경을 위한 토큰 생성 후 변경 링크를 메일로 전달
-     */
     public boolean sendResetEmailWithToken(String username) {
         if (userRepository.existsByUsername(username)) {
-            String uuid = resetTokenService.generateToken(username); // 일회용 토큰 생성
-            return mailService.generateEmail(username, uuid); // 토큰을 포함한 메일 생성
+            String token = generateToken(username);
+            return sendResetEmail(username, token);
         }
         return false;
     }
 
+    private String generateToken(String username) {
+        return resetTokenService.generateToken(username);
+    }
 
-    /**
-     * 토큰 유효성 검증 후 비밀번호 변경
-     */
+    private boolean sendResetEmail(String username, String token) {
+        return mailService.generateEmail(username, token);
+    }
+
     @Transactional
-    public boolean resetPwWithValidateToken(PasswordVO passwordVO) {
-        if (resetTokenService.isValidToken(passwordVO.getToken())) {
-            User user = userRepository.findByUsername(passwordVO.getUsername());
-            user.setPassword(bCryptPasswordEncoder.encode(passwordVO.getPassword()));
-            userRepository.save(user);
+    public boolean resetPwWithToken(PasswordVO passwordVO) {
+        if (isTokenValid(passwordVO.getToken())) {
+            resetPassword(passwordVO.getUsername(), passwordVO.getPassword());
+            inValidateToken(passwordVO.getToken());
 
-            resetTokenService.invalidateToken(passwordVO.getToken());
-
-            log.info("Reset password validated");
+            log.info("[{}] Reset Password Completed", passwordVO.getUsername());
             return true;
         }
         return false;
     }
 
-
-    /**
-     * 보안 질문 검증
-     * :비밀번호 변경 시 토큰 탈취 방지를 위한 2차 검증
-     */
-    public boolean checkSecureQuestion(SecureQuestionVO secureQuestionVO) {
-        User user = userRepository.findByUsername(secureQuestionVO.getUsername());
-        SecureAnswer secureAnswer = secureAnswerRepository.findBySecureQuestionIdAndUserId(
-                userRepository.findByUsername(secureQuestionVO.getUsername()).getSecureQuestion().getId(), user.getId());
-        return secureAnswer.getContent().equals(secureQuestionVO.getAnswer());
+    private boolean isTokenValid(String token) {
+        return resetTokenService.isValidToken(token);
     }
 
-
-    /**
-     * 특정 사용자의 보안 질문 조회
-     */
-    public String getSecureQuestion(String username) {
-        User user = userRepository.findByUsername(username);
-        return user.getSecureQuestion().getContent();
+    private void inValidateToken(String token) {
+        resetTokenService.invalidateToken(token);
     }
 
-
-    /**
-     * 사용자의 보안 질문과 답변 등록
-     */
-    @Transactional
-    public boolean registerSecureQuestion(User user, SecureQuestion question, String answerContent) {
-        try {
-            SecureAnswer answer = SecureAnswer.builder()
-                    .secureQuestion(question)
-                    .user(user)
-                    .content(answerContent)
-                    .build();
-            secureAnswerRepository.save(answer);
-            return true;
-        } catch(Exception e) {
-            throw new RuntimeException();
-        }
+    private void resetPassword(String username, String newPassword) {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("Username Not Found"));
+        user.setPassword(bCryptPasswordEncoder.encode(newPassword));
     }
 
-
-    /**
-     * 닉네임 조회
-     */
     public String getNickname() {
-        return userRepository.findByUsername(securityUtil.getCurrentUsername()).getNickname();
+        User user = userRepository.findByUsername(securityUtil.getCurrentUsername()).orElseThrow(() -> new RuntimeException("Username Not Found"));
+        return user.getNickname();
     }
 
 }
